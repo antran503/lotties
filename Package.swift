f4d701070f9743b9ef7b9ac42d386d1ef98b842f
Brandon Withrow <buba447@gmail.com>
2019-11-26 10:37:02 -0600

    Base Branch for supporting macos in SPM

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Lottie",
-    platforms: [.iOS(.v9)],
+    platforms: [.macOS("10.10")],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "Lottie", targets: ["Lottie"])
@@ -12,7 +12,7 @@ let package = Package(
         .target(
             name: "Lottie",
             path: "lottie-swift/src",
-            exclude: ["Public/MacOS"]
+            exclude: ["Public/iOS"]
         )
     ]
 )
